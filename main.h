22e1a4b24d2e922d4ebc99ab6c198d225857cca6
tluciad <tluciad@gmail.com>
2022-03-23 11:38:54 -0500

    main.h

--- main.h
+++ main.h
@@ -1,14 +1,21 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 
+#include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-typedef struct op
+/**
+ *struct print - Estruct
+ *@tp: format
+ *@f: function
+ */
+typedef struct print
 {
-	char *op;
+	char *tp;
 	int (*f)(va_list valist);
-} op_t;
+} p_t;
 
 int _putchar(char c);
 int _printf(const char *format, ...);
